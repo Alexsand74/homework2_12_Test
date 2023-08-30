0138713f09367a31f5540d63df7fe2f1e81f5113
Alexsand74 <1974alexsand@gmail.com>
2023-08-30 16:35:14 +0300

    Homework will be located here, 2.12 Testing.

---
+++ src/main/java/com/example/homework2_12_test/Homework212TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework2_12_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework212TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework212TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework2_12_test/Homework212TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_12_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework212TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
